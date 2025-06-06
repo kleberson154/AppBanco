90a5675482878894a1b7d188fc36e41d70991d24
kleberson154 <kleberson55@hotmail.com>
2025-06-06 16:26:05 -0300

    Adicionando os links do log in  para o Sign up

--- app/src/main/java/com/kleberson/appbanco/view/LoginActivity.kt
+++ app/src/main/java/com/kleberson/appbanco/view/LoginActivity.kt
@@ -1,14 +1,22 @@
-package com.kleberson.appbanco.view;
+package com.kleberson.appbanco.view
 
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.kleberson.appbanco.R
 
 class LoginActivity: AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(com.kleberson.appbanco.R.layout.login_activity)
+        setContentView(R.layout.login_activity)
+
+        val linkSignUp = findViewById<android.widget.TextView>(R.id.textViewLinkSignUp)
+
+        linkSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
