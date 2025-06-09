849070b3a6be58433c84e9e5364dc0c36080f9c8
kleberson154 <kleberson55@hotmail.com>
2025-06-09 15:15:03 -0300

    Criando a classe activity do Profile Create

---
+++ app/src/main/java/com/kleberson/appbanco/view/ProfileCreateActivity.kt
@@ -0,0 +1,26 @@
+package com.kleberson.appbanco.view
+
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.kleberson.appbanco.R
+import com.kleberson.appbanco.controller.AccountController
+
+class ProfileCreateActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_create_activity)
+
+        val firstName = findViewById<EditText>(R.id.editTextFirstName)
+        val lastName = findViewById<EditText>(R.id.editTextLastName)
+        val buttonSet = findViewById<Button>(R.id.buttonSet)
+
+        val accountController = AccountController()
+
+        buttonSet.setOnClickListener {
+            val firstNameText = firstName.text.toString()
+            val lastNameText = lastName.text.toString()
+            accountController.setName(firstNameText, lastNameText)
+        }
+    }
+}
