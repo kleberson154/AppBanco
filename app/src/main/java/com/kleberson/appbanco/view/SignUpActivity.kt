1972b18e9a2f2985b633f0e32d30e64d2a5f42ce
kleberson154 <kleberson55@hotmail.com>
2025-06-09 13:57:21 -0300

    Adicionando a funcionalidade de criar conta a view Sign Up

--- app/src/main/java/com/kleberson/appbanco/view/SignUpActivity.kt
+++ app/src/main/java/com/kleberson/appbanco/view/SignUpActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.kleberson.appbanco.R
+import com.kleberson.appbanco.controller.AccountController
 
 class SignUpActivity: AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -15,6 +16,20 @@ class SignUpActivity: AppCompatActivity() {
         setContentView(R.layout.signup_activity)
 
         val linkLogin = findViewById<TextView>(R.id.textViewLinkLogin)
+        val buttonSignUp = findViewById<TextView>(R.id.buttonSignUp)
+        val editTextEmail = findViewById<TextView>(R.id.editTextEmailAddress)
+        val editTextPassword = findViewById<TextView>(R.id.editTextPassword)
+        val editTextConfirmPassword = findViewById<TextView>(R.id.editTextConfirmPassword)
+
+        val accountController = AccountController()
+
+        buttonSignUp.setOnClickListener({
+            accountController.createAccount(
+                editTextEmail.text.toString(),
+                editTextPassword.text.toString(),
+                editTextConfirmPassword.text.toString(),
+            )
+        })
 
         linkLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
