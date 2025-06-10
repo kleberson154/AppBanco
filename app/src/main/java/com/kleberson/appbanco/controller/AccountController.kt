f881d6b3911217248cd7d61f00d868e14d002a24
kleberson154 <kleberson55@hotmail.com>
2025-06-10 13:37:09 -0300

    alterando o log para testes

--- app/src/main/java/com/kleberson/appbanco/controller/AccountController.kt
+++ app/src/main/java/com/kleberson/appbanco/controller/AccountController.kt
@@ -2,11 +2,13 @@ package com.kleberson.appbanco.controller
 
 import android.content.Context
 import android.util.Log
+import com.kleberson.appbanco.database.Database
 import com.kleberson.appbanco.model.Account
 
 
 class AccountController(private val context: Context) {
     var account: Account = Account("", "", "", "", "")
+    var db = Database(context)
 
     override fun toString(): String {
         Log.d("MVC_controller", "Controller iniciado")
@@ -31,12 +33,12 @@ class AccountController(private val context: Context) {
     fun setName(firstName: String, lastName: String) {
         account.firstName = firstName
         account.lastName = lastName
-        Log.d("MVC_controller", "Nome atualizado: $firstName $lastName")
+        Log.d("MVC_controller", "Dados salvos: ${account.toString()}")
     }
 
     fun setPhone(phone: String) {
         account.phone = phone
-        Log.d("MVC_controller", "Telefone atualizado: $phone")
+        Log.d("MVC_controller", "Dados salvos: ${account.toString()}")
     }
 
     fun confirmPassword(password: String, confirmPassword: String): Boolean {
