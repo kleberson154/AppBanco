5785d9b7c31c133ab6b7504042441514bc104c40
kleberson154 <kleberson55@hotmail.com>
2025-06-11 16:05:12 -0300

    adicionando os botoes de deposit, sake e Transfer

--- app/src/main/java/com/kleberson/appbanco/view/MainActivity.kt
+++ app/src/main/java/com/kleberson/appbanco/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -25,12 +26,23 @@ class MainActivity : AppCompatActivity() {
         val nameTextView = findViewById<TextView>(R.id.textViewShowNameMain)
         val balanceTextView = findViewById<TextView>(R.id.textViewBalance)
         val balanceCreditTextView = findViewById<TextView>(R.id.textViewBalanceCredit)
+        val buttonDeposit = findViewById<Button>(R.id.buttonDeposit)
+        val buttonSake = findViewById<Button>(R.id.buttonSake)
+        val buttonTransfer = findViewById<Button>(R.id.buttonTransfer)
         val profile = db.login(email, password)
 
         if (profile != null) {
             nameTextView.text = profile.firstName
             balanceTextView.text = formatBalance.format(profile.balance)
             balanceCreditTextView.text = formatBalance.format(profile.limitCredit)
+        }
+
+        buttonDeposit.setOnClickListener {
+            Log.d("MainActivity", "Deposit button clicked")
+            val intent = Intent(this, DepositActivity::class.java)
+            intent.putExtra("email", email)
+            intent.putExtra("password", password)
+            startActivity(intent)
         }
     }
 }
